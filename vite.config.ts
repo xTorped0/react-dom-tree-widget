a6f01ec72aa658f0d766aac416e36bfb7e0e8377
Torped0 <dabizha.vanya@gmail.com>
2024-02-03 18:30:19 +0200

    configuration file was changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: 'libs.js',
+        assetFileNames: 'styles.css'
+      }
+    },
   },
   server: {
     port: 8080,
